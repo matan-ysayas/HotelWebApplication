eaa07b3b3aad5faf1f481c8231d20f752b336dd7
matan-ysayas <matan.ysayas@gmail.com>
2022-01-18 12:19:05 +0200

    Add project files.

---
+++ HotelWebApplication/Controllers/CEOController.cs
@@ -0,0 +1,32 @@
+﻿using HotelWebApplication.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HotelWebApplication.Controllers
+{
+    public class CEOController : Controller
+    {
+        public List<CEO> CEOList = new List<CEO>() {new CEO (0,"matan ysayas",25,"kjhgg@gsk.cp.il",30000),new CEO(1,"lior",24,"jhhuu@gamil.com",20000) ,new CEO(2,"asci",24,"wewe@sdsd.com",40000),new CEO(3,"bob",25,"werr@ty.com",3000),new CEO(4,"avner",26,"eeee@dfdf.com",25000)};
+        
+        // GET: CEO
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult ShowCEOName()
+        {
+            return View(CEOList[4]);
+        }
+
+        public ActionResult ShowCEO(int id)
+        {
+            CEO CEOFound = CEOList.First((item) => item.Id == id);
+            
+            return View(CEOFound);
+        }
+    }
+}
---
+++ HotelWebApplication/Models/CEO.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HotelWebApplication.Models
+{
+    public class CEO
+    {
+        public int Id;
+        public string FullName;
+        public int Age;
+        public string Email;
+        public int Salary;
+
+        public CEO(int id, string fullName, int age, string email, int salary)
+        {
+            Id = id;
+            FullName = fullName;
+            Age = age;
+            Email = email;
+            Salary = salary;
+        }
+    }
+}
